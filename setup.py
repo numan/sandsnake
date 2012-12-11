579789bcc974acae3edf0559aa82d3db6fb786bd
Numan S <numan@7geese.com>
2012-12-11 01:04:55 -0800

    Wrong name. Copy and Paste ftl

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="sunspear",
+    name="sandsnake",
     license='Apache License 2.0',
     version="0.1.0ALPHA",
     description="Manage activity streams for objects.",
