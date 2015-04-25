8bab00249e520997497ce41f4979264a99ca4463
Numan S <numan856@gmail.com>
2015-04-24 17:56:01 -0700

    Change the order

--- sandsnake/backends/redis.py
+++ sandsnake/backends/redis.py
@@ -132,7 +132,7 @@ class Redis(BaseSandsnakeBackend):
             for index in indexes:
                 index_name = self._get_index_name(obj, index)
                 indexes_added.append(index_name)
-                conn.zadd(index_name, activity, timestamp)
+                conn.zadd(index_name, timestamp, activity)
                 conn.sadd(self._get_index_collection_name(obj), index)
 
         self._post_add(obj, indexes_added, activity, timestamp)
